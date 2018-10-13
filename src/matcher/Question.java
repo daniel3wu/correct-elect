c6128f9dba31a74405bfd11da75d0c9de36f753e
Taylor Shabani <taylor.shabani@duke.edu>
2018-10-13 18:08:34 -0400

    added quiz and question class

--- src/matcher/Question.java
+++ src/matcher/Question.java
@@ -2,4 +2,30 @@ package matcher;
 
 public class Question {
 
+	
+	int answer = 0;
+	
+	Question(String input){
+		
+		if (input.equals("Yes"))
+			answer = 1;
+		
+		else if (input.equals("No"))
+			answer = -1;
+		
+		else {
+			answer = 0;
+		}
+		
+	
+	
+	}
+	
+	public int getAnswer() {
+		return answer;
+	}
+	
+	
+	
+
 }
--- src/matcher/Quiz.java
+++ src/matcher/Quiz.java
@@ -1,5 +1,31 @@
 package matcher;
 
-public class Quiz {
+import java.util.ArrayList;
 
+public class Quiz {
+	int size = 0; 
+	ArrayList<Question> questions = new ArrayList<Question>(); 
+	
+	
+	Quiz(int size){
+		this.setAnswers(size);
+		
+		
+	}
+	
+	public void setAnswers(int size) {
+		for (int i =0; i < size; i++) {
+		//	Question q = new Question();//enter input
+		//	questions.add(q);
+					
+		}
+		
+		
+	}
+
+	public ArrayList<Question> getAnswers (){
+		return questions;
+	}
+	
+	
 }
