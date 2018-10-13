fdd2824435a5272a9d2a5af1d6a9144acb9b4c2e
pmohamma <pmohamma@trinity.edu>
2018-10-13 17:32:39 -0400

    working on politician class

--- src/matcher/Politician.java
+++ src/matcher/Politician.java
@@ -1,5 +1,27 @@
 package matcher;
 
+import java.util.HashMap;
+
 public class Politician {
+	private String myName;
+	private String myId;
 	
+	private Double nraRating;
+	private Double enviroRating;
+	
+	private HashMap<String, Double> sigRatings = new HashMap<String, Double>();
+	
+	public Politician(String name) {
+		myName = name;
+		
+	}
+	
+	public String getName() {
+		return myName;
+	}
+	
+	private Double getRating(int myId, int sigId) {
+		//use api and the getCandidateRating function to return the candidates rating with the NRA
+		return 0.0;
+	}
 }
