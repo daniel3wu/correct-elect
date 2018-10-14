953e7d467a35cdf5ea3cd19776e16c13c6792c9a
pmohamma <pmohamma@trinity.edu>
2018-10-14 10:29:49 -0400

    user and politician implemented

---
+++ src/object/Person.java
@@ -0,0 +1,56 @@
+package object;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Person {
+	protected String myName;
+	protected String myId;
+	
+	protected int gunRating;
+	protected int lgbtqRating;
+	protected int enviroRating;
+	protected int abortionRating;
+	protected int healthcareRating;
+	
+	public enum Party {DEMOCRAT, REPUBLICAN, LIBRETARIAN, GREEN}
+	protected static final ArrayList<Party> parties = new ArrayList<Party> (Arrays.asList(Party.values()));
+	
+	public enum Office {SENATOR, REPRESENTATIVE, PRESIDENT, GOVERNOR, MAYOR}
+	protected static final ArrayList<Office> offices = new ArrayList<Office> (Arrays.asList(Office.values()));
+
+	protected int myZip;
+	
+	String[] states = {"Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"};
+	
+	protected HashMap<String, Integer> opinions = new HashMap<String, Integer> ();
+	
+	public String getName() {
+		return myName;
+	}
+	
+	public int getZip() {
+		return myZip;
+	}
+	
+	public int getGunRating() {
+		return gunRating;
+	}
+	public int getLgbtqRating() {
+		return lgbtqRating;
+	}
+	public int getEnviroRating() {
+		return enviroRating;
+	}
+	public int getAbortionRating() {
+		return abortionRating;
+	}
+	public int getHealthcareRating() {
+		return healthcareRating;
+	}
+	
+	public int getRating(String s) {
+		return opinions.get(s);
+	}
+}
--- src/object/Politician.java
+++ src/object/Politician.java
@@ -1,40 +1,18 @@
 package object;
 
-import java.awt.List;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Random;
 
-public class Politician {
-	private String myName;
-	private String myId;
+public class Politician extends Person{
 	
-	
-	private Double nraRating;
-	private int gunRating;
-	private int lgbtqRating;
-	private int enviroRating;
-	private int abortionRating;
-	private int healthcareRating;
-	
-	public enum Party {DEMOCRAT, REPUBLICAN, LIBRETARIAN, GREEN}
-	private static final ArrayList<Party> parties = new ArrayList<Party> (Arrays.asList(Party.values()));
-	
-	public enum Office {SENATOR, REPRESENTATIVE, PRESIDENT, GOVERNOR, MAYOR}
-	private static final ArrayList<Office> offices = new ArrayList<Office> (Arrays.asList(Office.values()));
-	
+	//private Double nraRating;
+
 	private Party myParty;
 	private Office myOffice;
-	private int myZip;
 	
 	Random rand = new Random();
 	
-	String[] states = {"Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"};
-	
 	private HashMap<String, Double> sigRatings = new HashMap<String, Double>();
-	private HashMap<String, Integer> opinions = new HashMap<String, Integer> ();
 	
 	public Politician(String name) {
 		myName = name;
@@ -47,11 +25,7 @@ public class Politician {
 		myOffice = offices.get(rand.nextInt(parties.size()));
 		myZip = rand.nextInt(89999)+10000;
 	}
-	
-	public String getName() {
-		return myName;
-	}
-	
+		
 	public Office getOffice() {
 		return myOffice;
 	}
@@ -59,11 +33,7 @@ public class Politician {
 	public Party getParty() {
 		return myParty;
 	}
-	
-	public int getZip() {
-		return myZip;
-	}
-	
+		
 	private void initializeRatings() {
 		gunRating = rand.nextInt(3);
 		lgbtqRating = rand.nextInt(3);
@@ -75,22 +45,6 @@ public class Politician {
 		opinions.put("environment", enviroRating);
 		opinions.put("abortion", abortionRating);
 		opinions.put("healthcare", healthcareRating);
-	}
-	
-	public int getGunRating() {
-		return gunRating;
-	}
-	public int getLgbtqRating() {
-		return lgbtqRating;
-	}
-	public int getEnviroRating() {
-		return enviroRating;
-	}
-	public int getAbortionRating() {
-		return abortionRating;
-	}
-	public int getHealthcareRating() {
-		return healthcareRating;
 	}
 	 
 	private double getSigRating(int sigId, String sigName) {
